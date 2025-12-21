10131b8d9b47568a100321e1cef7ecfd75319d84
Patricio Scidá <patricioscida@gmail.com>
2025-12-21 12:14:24 -0300

    Se agrega el endpoint categoria

--- BackEndAPI/Repositories/CategoriasRepository.cs
+++ BackEndAPI/Repositories/CategoriasRepository.cs
@@ -27,5 +27,34 @@ namespace BackEndAPI.Repositories
             await db.SaveChangesAsync();
             return nuevaCategoria;
         }
+
+        public async Task<IEnumerable<Categoria>> GetAllCategorias()
+        {
+            return await db.Categorias.ToListAsync();
+        }
+
+        public async Task<Categoria?> GetCategoriaById(Guid id)
+        {
+            return await db.Categorias.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Categoria?> ActualizarCategoria(Categoria categoria)
+        {
+            db.Entry(categoria).State = EntityState.Modified;
+            await db.SaveChangesAsync();
+            return null;
+        }
+
+        public async Task<Categoria?> EliminarCategoria(Guid id)
+        {
+            var categoria = await db.Categorias.FirstOrDefaultAsync(c => c.Id == id);
+            if (categoria == null)
+            {
+                return null;
+            }
+            db.Categorias.Remove(categoria);
+            await db.SaveChangesAsync();
+            return categoria;
+        }
     }
 }
