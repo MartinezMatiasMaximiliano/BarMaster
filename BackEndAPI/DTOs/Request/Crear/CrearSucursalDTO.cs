6b8f298a5376019b270f120ae078dede584376f1
DevMartinez <matumartinez9@hotmail.com>
2026-02-03 03:40:27 -0300

    endpoint Actualizar sucursales

--- BackEndAPI/DTOs/Request/Crear/CrearSucursalDTO.cs
+++ BackEndAPI/DTOs/Request/Crear/CrearSucursalDTO.cs
@@ -5,6 +5,7 @@
         public string Nombre { get; set; } = null!;
         public string Direccion { get; set; } = null!;
         public string? Telefono { get; set; } = null!;
+        public string Username { get; set; } = null!; 
         public string Password { get; set; } = null!;
 
     }
