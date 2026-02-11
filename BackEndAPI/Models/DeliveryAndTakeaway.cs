9a0f5b1a2ddfb93d6d99a04634d9766714ff9b3b
DevMartinez <matumartinez9@hotmail.com>
2026-02-11 10:50:59 -0300

    cambios DB, fecha ProductosPorVisita y DeliveryTakeaway

--- BackEndAPI/Models/DeliveryAndTakeaway.cs
+++ BackEndAPI/Models/DeliveryAndTakeaway.cs
@@ -1,16 +1,16 @@
 ﻿namespace BackEndAPI.Models
 {
-    public class Delivery
+    public class DeliveryAndTakeaway
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public Guid IdSucursal { get; set; }
         //public Guid IdPersonaRegistro { get; set; }
         //public Guid IdCadete { get; set; }
-        public int IdTipoEnvio { get; set; }
+        public int? IdTipoEnvio { get; set; }
         public Guid IdVisita { get; set; }
         public DateTime FechaHora { get; set; } = DateTime.Now;
         public string NombreCliente { get; set; } = null!;
-        public string Direccion { get; set; } = null!;  
+        public string? Direccion { get; set; } = null;  
         public string? Indicaciones { get; set; } 
         public string Telefono { get; set; } = null!;
         public decimal PrecioTotal { get; set; }
