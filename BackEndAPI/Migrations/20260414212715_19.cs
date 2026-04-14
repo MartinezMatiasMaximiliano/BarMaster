875f70d6d680d763d05bcf52e677706d1c30cff5
DevMartinez <matumartinez9@hotmail.com>
2026-04-14 18:28:02 -0300

    Columna para nombre de Mesa en Reservas

---
+++ BackEndAPI/Migrations/20260414212715_19.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackEndAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class _19 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MesaReserva",
+                table: "Reservas",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MesaReserva",
+                table: "Reservas");
+        }
+    }
+}
