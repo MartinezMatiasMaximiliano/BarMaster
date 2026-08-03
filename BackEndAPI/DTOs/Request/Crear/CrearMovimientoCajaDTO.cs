40ac8f105b97487250cd7d2b2918fd24785fa227
Patricio Scidá <patricioscida@gmail.com>
2026-08-03 18:28:25 -0300

    descripción tiene que ser opcional

--- BackEndAPI/DTOs/Request/Crear/CrearMovimientoCajaDTO.cs
+++ BackEndAPI/DTOs/Request/Crear/CrearMovimientoCajaDTO.cs
@@ -5,7 +5,7 @@ namespace BackEndAPI.DTOs.Request.Crear
         public int IdTipoMovimientoCaja { get; set; }
         public Guid IdCaja { get; set; }
         public decimal Monto { get; set; }
-        public string Descripcion { get; set; } = string.Empty;
+        public string? Descripcion { get; set; }
     }
 }
 
