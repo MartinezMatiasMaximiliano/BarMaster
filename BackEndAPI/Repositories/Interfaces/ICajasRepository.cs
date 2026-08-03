34fb6b477e2c33a5f785923ae99052c24495ae79
Patricio Scidá <patricioscida@gmail.com>
2026-08-03 17:03:58 -0300

    apertura caja usa el montocierre de la caja anterior

--- BackEndAPI/Repositories/Interfaces/ICajasRepository.cs
+++ BackEndAPI/Repositories/Interfaces/ICajasRepository.cs
@@ -7,6 +7,7 @@ namespace BackEndAPI.Repositories.Interfaces
         Task<Caja> CrearCaja(Caja caja);
         Task<Caja?> BuscarCajaAbierta();
         Task<Caja?> BuscarCajaAbiertaPorIdSucursal(Guid IdSucursal);
+        Task<Caja?> BuscarUltimaCajaCerradaPorIdSucursal(Guid IdSucursal);
         Task<Caja?> GetCajaPorId(Guid id);
         Task<List<Caja>> BuscarListaCajas();
         Task<Caja> ActualizarCaja(Caja caja);
