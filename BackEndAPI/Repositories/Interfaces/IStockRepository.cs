c5b5b09a9f87f7853a62847cffaee7a1121179a2
Patricio Scidá <patricioscida@gmail.com>
2026-08-26 13:37:24 -0300

    endpoint para obtener alertas de stock

--- BackEndAPI/Repositories/Interfaces/IStockRepository.cs
+++ BackEndAPI/Repositories/Interfaces/IStockRepository.cs
@@ -1,3 +1,4 @@
+using BackEndAPI.DTOs.Response;
 using BackEndAPI.Models;
 
 namespace BackEndAPI.Repositories.Interfaces
@@ -5,6 +6,7 @@ namespace BackEndAPI.Repositories.Interfaces
     public interface IStockRepository
     {
         Task<IReadOnlyCollection<StockProductoSucursal>> ObtenerStockAsync(Guid idSucursal);
+        Task<IReadOnlyCollection<AlertaStockDTO>> ObtenerAlertasAsync(Guid idSucursal);
         Task<IReadOnlyCollection<MovimientoStock>> ObtenerMovimientosAsync(Guid idProducto, Guid idSucursal);
         Task<StockProductoSucursal> ConfigurarAsync(Guid idProducto, Guid idSucursal, bool controlaStock, bool enviarAlerta, int cantidadMinima, int? cantidadInicial);
         Task<StockProductoSucursal> RegistrarMovimientoAsync(Guid idProducto, Guid idSucursal, int cantidad, string? motivo);
